72822e0fe9866fc4271dba8d6e5d90d0b17cc954
kirtikayush <kirtikayush2000@gmail.com>
2025-10-04 01:27:40 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import MenuBar from "./components/menubar/MenuBar";
 import Hero from "./components/hero/Hero";
 import LoginPage from "./components/authpage/LoginPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
